53c059d2a4ac9e77c9ffbd28b708ba1139228833
sharp-lilac <161522137+sharp-lilac@users.noreply.github.com>
2024-11-01 14:04:37 -0600

    Create theme.R to modularize code
    Define shiny dashboard theme inside new theme.R script to modularize code and simplify ui.R

---
+++ theme.R
@@ -0,0 +1,23 @@
+## theme.R
+
+# Define shiny dashboard theme ---------------------------
+library(fresh)
+dash_theme <- create_theme(
+    adminlte_color(
+        light_blue = "#5E81AC",
+        aqua = "#88C0D0",
+        green = "#A3BE8C",
+        purple = "#B48EAD"
+    ),
+    adminlte_sidebar(
+        width = "200px",
+        dark_bg = "#4C566A",
+        dark_hover_bg = "#88C0D0",
+        dark_color = "#ECEFF4" # Light text for readability
+    ),
+    adminlte_global(
+        content_bg = "#ECEFF4",
+        box_bg = "#D8DEE9",
+        info_box_bg = "#A3BE8C"
+    )
+)
--- ui.R
+++ ui.R
@@ -5,28 +5,9 @@ library(shiny)
 library(shinydashboard)
 library(shinyWidgets)
 library(shinycssloaders)
-library(fresh) # custom themes for dashboard
 
-# Define theme ---------------------------
-dash_theme <- create_theme(
-    adminlte_color(
-        light_blue = "#5E81AC",
-        aqua = "#88C0D0",
-        green = "#A3BE8C",
-        purple = "#B48EAD",
-    ),
-    adminlte_sidebar(
-        width = "200px",
-        dark_bg = "#4C566A",
-        dark_hover_bg = "#88C0D0",
-        dark_color = "#ECEFF4" # Light text for readabilit
-    ),
-    adminlte_global(
-        content_bg = "#ECEFF4",
-        box_bg = "#D8DEE9",
-        info_box_bg = "#A3BE8C"
-    )
-)
+# Source Objects ---------------------------
+source("theme.R")
 
 # Define ui ---------------------------
 ui <- dashboardPage(
