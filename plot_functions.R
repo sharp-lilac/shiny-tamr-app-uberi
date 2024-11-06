b53b7d5f725e30d5db2ea2e5e0a4e93a41da9a6f
sharp-lilac <161522137+sharp-lilac@users.noreply.github.com>
2024-11-06 13:41:22 -0600

    Means change by locality, coral species cover plot

--- plot_functions.R
+++ plot_functions.R
@@ -7,16 +7,17 @@ library(ggplot2)
 create_coral_cover_year_plot <- function(data_filtered, input, caption) {
     base_plot <- ggplot(data_filtered, aes(x = Year, y = Percent_Coral)) +
         geom_boxplot(color = "black", position = position_dodge(width = 0.75), outlier.shape = 4, outlier.size = 4) +
-        stat_summary(fun = mean, geom = "point", shape = 18, size = 3, fill = "#3B4252", position = position_dodge(width = 0.75)) +
-        labs(caption = caption, x = "Percent Coral Cover") +
+        labs(caption = caption, y = "Percent Coral Cover") +
         theme_classic() +
         gg_theme +
         scale_y_continuous(breaks = seq(0, 100, by = 10), sec.axis = dup_axis(name = ""))
     if (input$coral_cover_year_consolidate_locality) {
-        base_plot
+        base_plot +
+            stat_summary(fun = mean, geom = "point", shape = 23, fill = "#3B4252", size = 3, position = position_dodge(width = 0.75))
     } else {
         base_plot +
             aes(fill = Locality) +
+            stat_summary(aes(fill = Locality), fun = mean, geom = "point", shape = 23, size = 3, position = position_dodge(width = 0.75)) +
             scale_fill_manual(name = "Locality", values = palette, guide = guide_legend(nrow = 2))
     }
 }
@@ -25,21 +26,24 @@ create_coral_cover_year_plot <- function(data_filtered, input, caption) {
 create_coral_cover_species_plot <- function(data_filtered, input, caption) {
     base_plot <- ggplot(data_filtered, aes(x = reorder(Organism, -Percent, FUN = mean), y = Percent)) +
         geom_boxplot(color = "black", position = position_dodge(width = 0.75), outlier.shape = 4, outlier.size = 4) +
-        stat_summary(fun = mean, geom = "point", shape = 18, size = 3, fill = "#3B4252", position = position_dodge(width = 0.75)) +
-        labs(caption = caption, x = "Percent Cover") +
+        labs(caption = caption, y = "Percent Cover", x = "Coral Species Code") +
         theme_classic() +
         gg_theme +
-        theme(axis.text.x = element_text(angle = 90)) +
         scale_y_continuous(breaks = seq(0, 100, by = 10), sec.axis = dup_axis(name = ""))
     if (input$coral_cover_species_color_toggle == "Neither") {
-        return(base_plot)
+        base_plot +
+            stat_summary(fun = mean, geom = "point", shape = 23, size = 3, fill = "#3B4252", position = position_dodge(width = 0.75))
     } else if (input$coral_cover_species_color_toggle == "Locality") {
         base_plot +
             aes(fill = Locality) +
-            scale_fill_manual(name = "Locality", values = palette, guide = guide_legend(nrow = 2))
+            stat_summary(aes(fill = Locality), fun = mean, geom = "point", shape = 23, size = 3, position = position_dodge(width = 0.75)) +
+            scale_fill_manual(name = "Locality", values = palette, guide = guide_legend(nrow = 2)) +
+            scale_color_manual(values = palette)
     } else {
         base_plot +
             aes(fill = Year) +
-            scale_fill_manual(name = "Year", values = palette, guide = guide_legend(nrow = 2))
+            stat_summary(aes(fill = Year), fun = mean, geom = "point", shape = 23, size = 3, position = position_dodge(width = 0.75)) +
+            scale_fill_manual(name = "Year", values = palette, guide = guide_legend(nrow = 2)) +
+            scale_color_manual(values = palette)
     }
 }
