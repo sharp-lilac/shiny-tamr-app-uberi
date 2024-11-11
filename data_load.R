23d549ddbc305ed17098fc8b76a4f4006afda31f
Jess Boles <161522137+sharp-lilac@users.noreply.github.com>
2024-11-11 14:20:36 -0600

    Add restructured fish data to repository for app optimization (#61)
    * Add fish biomass dataset
    
    * Add fish size/count dataframes

--- data_load.R
+++ data_load.R
@@ -1,7 +1,7 @@
 ## data_load.R
 
 # Load packages ---------------------------
-library(vroom) # for fast data loading
+library(vroom)
 
 # Define NA vector ---------------------------
 nas <- c("", " ", "NA", "U", "NA ")
@@ -9,9 +9,10 @@ nas <- c("", " ", "NA", "U", "NA ")
 # Load data ---------------------------
 df_master_benthic <- vroom::vroom("data/dryad/Master_Benthic_PIM_2010-2023.csv", na = nas)
 df_master_coral <- vroom::vroom("data/dryad/Master_Coral_Community_2010-2021.csv", na = nas)
-df_master_fish <- vroom::vroom("data/dryad/Master_Fish_Survey_2010-2023.csv", na = nas)
+df_master_fish_biomass <- vroom::vroom("data/dryad/restructured/Master_Fish_Biomass.csv", na = nas)
+df_master_fish_size <- vroom::vroom("data/dryad/restructured/Master_Fish_Size.csv", na = nas)
+df_master_fish_count <- vroom::vroom("data/dryad/restructured/Master_Fish_Count.csv", na = nas)
 df_ref_disease <- vroom::vroom("data/dryad/Ref_Diseases_Coral.csv", na = nas)
 df_ref_collec <- vroom::vroom("data/dryad/Ref_Collectors_Turneffe.csv", na = nas)
 df_ref_organisms <- vroom::vroom("data/dryad/Ref_Organisms_Benthic.csv", na = nas)
 df_ref_sites <- vroom::vroom("data/dryad/Ref_Sites_Turneffe.csv", na = nas)
-df_ref_biomass <- vroom::vroom("data/reference/fish_biomass_specification.csv", na = nas)
